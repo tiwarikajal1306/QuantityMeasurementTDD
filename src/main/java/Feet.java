34c3edcc8b3f51d06f6f5c1fe0fd62467d6c888b
kajal <tiwarikajal1306@gmail.com>
2020-07-13 16:57:52 +0530

    ADDED Code to check equality of 2 two test values

--- src/main/java/Feet.java
+++ src/main/java/Feet.java
@@ -1,4 +1,15 @@
 public class Feet {
-    public Feet(double v) {
-    }
+        private double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Feet feet = (Feet) o;
+            return Double.compare(feet.value, value) == 0;
+        }
 }
